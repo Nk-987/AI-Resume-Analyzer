4220a2384798aad2823e8f4ce10701d8532ce95c
itsnk <107532950+Nk-987@users.noreply.github.com>
2026-03-16 13:36:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 nltk.download('punkt')
+nltk.download('punkt_tab')
 nltk.download('stopwords')
 
 from nltk.corpus import stopwords
